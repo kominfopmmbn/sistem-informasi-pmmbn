d39f9f83afd36ffea39bff30fbee549a13582f6e
Fadoilul Mun`im <50235124+fadoilulmun-im@users.noreply.github.com>
2026-05-04 21:47:48 +0700

    Refactor navbar blade template to improve the structure of the 'Download' link, enhancing readability and maintainability.

--- resources/views/front/layouts/partials/navbar.blade.php
+++ resources/views/front/layouts/partials/navbar.blade.php
@@ -36,7 +36,7 @@
                     </ul>
                 </li>
                 {{-- <li class="nav-item"><a class="nav-link" href="#">Program Unggulan</a></li> --}}
-                <li class="nav-item {{ request()->routeIs('download.index') ? 'active text-brand' : '' }}"><a class="nav-link" href="{{ route('download.index') }}">Download</a></li>
+                <li class="nav-item"><a class="nav-link {{ request()->routeIs('download.index') ? 'active text-brand' : '' }}" href="{{ route('download.index') }}">Download</a></li>
             </ul>
         </div>
     </div>
